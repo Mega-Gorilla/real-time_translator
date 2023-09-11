a2593d59b9a8eb159a4b77b8b4396f039483b23a
Mega Gorilla <hahahadesu@gmail.com>
2023-09-12 00:03:02 +0900

    sounddeviceを実装

--- get_audio_device_list.py
+++ get_audio_device_list.py
@@ -1,6 +1,7 @@
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from pycaw.pycaw import AudioUtilities
+import sounddevice as sd
 
 def show_all_audio_device():
 
@@ -17,6 +18,15 @@ def Return_VB_Audio_ID():
         if "CABLE Output (VB-Audio Virtual Cable)" in f"{endpoint.FriendlyName}":
             return endpoint.id
         
+def find_device_id_SD(target_name):
+    devices = sd.query_devices()
+    for device in devices:
+        if target_name in device['name'] :
+            return device['index']
+    return None
+        
 if __name__ == "__main__":
     #show_all_audio_device()
-    Return_VB_Audio_ID()
+    #Return_VB_Audio_ID()
+    device_id = find_device_id_SD("CABLE Output (VB-Audio Virtual Cable)")
+    print(device_id)
